7bf7f7338d1260cbd32e5c2c793273b20cc68d2f
brainmaze <devrin.hoefer@gmail.com>
2025-01-07 23:12:20 -0800

    yes commit

--- script.js
+++ script.js
@@ -2,37 +2,37 @@ const questions = [
     {
     question: "Which is the largest animal in the world?",
     answers:[
-    {text:"Shark", Correct: false},
-    {text:"Blue whale", Correct: true},
-    {text:"Elephant", Correct: false},
-    {text:"Giraffe", Correct: false},
+    {text:"Shark", correct: false},
+    {text:"Blue whale", correct: true},
+    {text:"Elephant", correct: false},
+    {text:"Giraffe", correct: false},
             ]
     },
     {
         question: "Which is the smallest country in the world?",
         answers:[
-        {text:"Vatican City", Correct: true},
-        {text:"Bhutan", Correct: false},
-        {text:"Nepal", Correct: false},
-        {text:"Shri Lanka", Correct: false},
+        {text:"Vatican City", correct: true},
+        {text:"Bhutan", correct: false},
+        {text:"Nepal", correct: false},
+        {text:"Shri Lanka", correct: false},
                 ]  
     },      
     {
         question: "Which is the largest desert in the world?",
         answers:[
-        {text:"Kalahari", Correct: false},
-        {text:"Gobi", Correct: false},
-        {text:"Sahara", Correct: false},
-        {text:"Antartica", Correct: true},
+        {text:"Kalahari", correct: false},
+        {text:"Gobi", correct: false},
+        {text:"Sahara", correct: false},
+        {text:"Antartica", correct: true},
                 ]
         },
         {
             question: "Which is the smallest continent in the world?",
             answers:[
-            {text:"Asia", Correct: false},
-            {text:"Australia", Correct: true},
-            {text:"Arctic", Correct: false},
-            {text:"Africa", Correct: false},
+            {text:"Asia", correct: false},
+            {text:"Australia", correct: true},
+            {text:"Arctic", correct: false},
+            {text:"Africa", correct: false},
                     ]  
         } 
 ];
