c389b11458ac166ce63d1e902ac25ccf53615a77
brainmaze <devrin.hoefer@gmail.com>
2025-01-15 22:29:59 -0800

    yes commit

--- script.js
+++ script.js
@@ -108,7 +108,7 @@ function selectAnswer(e){
 function showScore(){
     resetState();
     questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
-    nextButton.innerHTML = "Play Again";
+    nextButton.innerHTML = "Next";
     nextButton.style.display = "block";
 }
 
