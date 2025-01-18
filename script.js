c25f935eb235b9153d6b08fbab43a0bc54dc75d4
brainmaze <devrin.hoefer@gmail.com>
2025-01-18 15:24:46 -0800

    yes

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ function selectAnswer(e){
     nextButton.style.display = "block";
 }
 
-function showScore(){
+export function showScore(){
     resetState();
     questionElement.innerHTML = `You scored ${score} out of ${questions.length} a percentage of ${(score/questions.length)*100}%!`;
     nextButton.innerHTML = "Exit";
@@ -130,6 +130,29 @@ nextButton.addEventListener("click", ()=>{
     }
 });
 
+const startingMinutes = 5;
 
+
+const countdownEl = document.getElementById('countdown');
+
+let time = startingMinutes * 60; //minutes * 60 seconds
+let refreshIntervalId = setInterval(updateCountdown, 1000);
+
+function updateCountdown(){
+
+    const minutes = Math.floor(time/60);
+    let seconds = time % 60;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    const contdownEl = document.getElementById("f1"); 
+    countdownEl.innerHTML = `${minutes} : ${seconds}`;
+    time--;
+
+    if (time < 0) { //stop the setInterval whe time = 0 for avoid negative time
+        clearInterval(refreshIntervalId);
+        showScore();
+}
+
+}
 
 startQuiz();
+
--- timer.js
+++ timer.js
@@ -1,3 +1,8 @@
+
+
+
+
+
 const startingMinutes = 5;
 
 
