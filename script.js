4c0dd1bb7d43b9e467f7a21ba90681b6fcf58b8e
brainmaze <devrin.hoefer@gmail.com>
2025-01-20 08:41:01 -0800

    yes

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-
 setTimeout(function() {
 
     showScore()
     }, 602000);
-
-
+    
+    const startingMinutes = 10;
+    const countdownEl = document.getElementById('countdown');
+    
+    let time = startingMinutes * 60; //minutes * 60 seconds
+    let refreshIntervalId = setInterval(updateCountdown, 1000);
+    
+    function updateCountdown(){
+    
+        const minutes = Math.floor(time/60);
+        let seconds = time % 60;
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+        const contdownEl = document.getElementById("f1"); 
+        countdownEl.innerHTML = `${minutes} : ${seconds}`;
+        time--;
+    
+        if (time < 0) { //stop the setInterval whe time = 0 for avoid negative time
+            clearInterval(refreshIntervalId);
+                        }
+    
+    
+    
+    };
+    
+updateCountdown();
 
 const questions = [
     {
@@ -121,19 +143,32 @@ function handleNextButton(){
         showQuestion();
     }else{
         showScore();
+        endtimer();
+        clearInterval(refreshIntervalId);
     }
 }
 
+function endtimer(){
+    if(currentQuestionIndex === 3){
+        
+        console.log("nada")
+    }
+}
+function mathlink(){
+
+    location.href = "https://www.google.com";
+
+}
 
 nextButton.addEventListener("click", ()=>{
     if(currentQuestionIndex < questions.length){
         handleNextButton();
+        
     }else{
-        startQuiz();
+        
+       mathlink(); 
     }
 });
 
 
-
 startQuiz();
-
