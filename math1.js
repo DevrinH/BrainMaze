3e48fed426bb4b57566e622ec589755d4da47843
brainmaze <devrin.hoefer@gmail.com>
2025-01-21 22:12:44 -0800

    yes

--- math1.js
+++ math1.js
@@ -1,3 +1,6 @@
+let readScore = localStorage.getItem("readingScore");
+let readingWritingScore = Number(readScore);
+
 setTimeout(function() {
 
     showScore();
@@ -32,13 +35,13 @@ updateCountdown();
 const questions = [
     {
         
-        question: " The recovery of southern sea otters appears to have taken an upturn, according to results from the annual California sea otter survey released by the U.S. Geological Survey. Yet despite an overall increase in sea otter abundance, sharks have been “taking a bite” out of the portion of the population that could fuel expansion into new areas.<br/><br/>“There’s much more to the story here than the main finding would suggest,” said Dr. Tim Tinker, a research ecologist who leads the USGS sea otter research program. “We are looking into various factors that may be affecting the survey results, including a boom in urchin abundance from Big Sur to Monterey that may explain the uptick in numbers in the range center, and high levels of shark bite mortality that are likely responsible for continued declines at the north and south ends of the range.<br/><br/>This year’s survey results suggest an increasing trend over the last five years of almost 2 percent per year and the population index, a statistical representation of the entire population calculated as the three-year running average of census counts, has climbed to 3,054 from 2,711 in 2010. The growth is accounted for by an unexpected jump in numbers in the center of the sea otter’s range, an area that spans from Monterey south to Cambria.<br/><br/>While the population index continues to trend upward, the northern and southern subsets of the population continue a five-year decline, dropping 2 percent and 3.4 percent per year, respectively, numbers consistent with increased shark bite induced mortality in these same areas.<br/><br/>Since the 1980s, USGS scientists have computed the annual population index and evaluated trends in the southern sea otter, “Enhydra lutris nereis,” a federally listed threatened species found in California. For southern sea otters to be considered for removal from threatened species listing under the Endangered Species Act, the population index would have to exceed 3,090 for three consecutive years.<br/><br/>Question:<br/>The passage characterizes the main finding of the 2015 sea otter survey conducted by the US Geological Survey as",
+        question: "Maria is staying at a hotel that charges $99.95 per night plus tax for a room. A tax of 8% is applied to the room rate, and an additional onetime untaxed fee of $5.00 is charged by the hotel. Which of the following represents Maria’s total charge, in dollars, for staying x nights?",
        
         answers: [
-            { text: "initially promising, because it shows that the southern sea otter will not become an endangered species.", correct: false},
-            { text: "very significant, because it validates the recent efforts of sea otter conservation groups working along the California coast.", correct: false},
-            { text: "ultimately inconclusive, because it does not incorporate data from sea otter population surveys conducted before 2010.", correct: false},
-            { text: "somewhat misleading, because it does not make it clear that the southern sea otter population trends varied throughout the range.", correct: true},
+            { text: "1.08(99.95 + 5)x", correct: false},
+            { text: "1.08(99.95x + 5)", correct: false},
+            { text: "(99.95 + 0.08x) + 5", correct: false},
+            { text: "1.08(99.95x) + 5", correct: true},
         ]
     },
     {
@@ -78,11 +81,11 @@ const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
-let score = 0;
+let mathScore = 0;  //score
 
 function startQuiz(){
     currentQuestionIndex = 0;
-    score = 0;
+    mathScore = 0;  //score
     nextButton.innerHTML = "Next";
     showQuestion();
 }
@@ -110,6 +113,7 @@ function resetState(){
     nextButton.style.display = "none";
     while(answerButtons.firstChild){
         answerButtons.removeChild(answerButtons.firstChild);
+        
     }
 }
 
@@ -118,7 +122,7 @@ function selectAnswer(e){
     const isCorrect = selectedBtn.dataset.correct === "true";
     if(isCorrect){
         selectedBtn.classList.add("correct");
-        score++;
+        mathScore++; //score
     }else{
         selectedBtn.classList.add("incorrect");
     }
@@ -133,11 +137,12 @@ function selectAnswer(e){
 
 function showScore(){
     resetState();
-    questionElement.innerHTML = `You scored ${score} out of ${questions.length} a percentage of ${(score/questions.length)*100}%!`;
+    questionElement.innerHTML = `You scored ${mathScore} out of ${questions.length} and your reading score was ${readingWritingScore}!`; //score
     nextButton.innerHTML = "Continue";
     nextButton.style.display = "block";
-    
+    return true;
 }
+
 function handleNextButton(){
     currentQuestionIndex++;
     if(currentQuestionIndex < questions.length && time > 0){
@@ -157,7 +162,7 @@ function endtimer(){
 }
 function mathlink(){
 
-    location.href = "https://www.brainsmaze.com/math.html";
+    location.href = "https://www.brainsmaze.com/";
 
 }
 
@@ -166,7 +171,7 @@ nextButton.addEventListener("click", ()=>{
         handleNextButton();
         
     }else{
-        localStorage.setItem("readingScore", score); 
+        
         mathlink(); 
     }
 });
