7bca0abdd69ebe2a6881161e0e71b072c354bd80
brainmaze <devrin.hoefer@gmail.com>
2025-01-28 21:54:54 -0800

    yes

--- mainpagequiz.js
+++ mainpagequiz.js
@@ -11,31 +11,31 @@ const questions = [
         ]
     },
     {
-        question: " Question 2",
+        question: " The United Nations High Seas Treaty is a multilateral agreement that provides unprecedented protection for ocean areas that have no particular legal jurisdiction. Overfishing, seabed mining, and other exploitative measures will be blocked by the new ________ marine protected areas to be established with the support of multiple countries. Negotiated at the United Nations New York headquarters, the treaty was agreed upon by global ambassadors of marine affairs and foreign affairs.<br/>Which choice completes the text so that it conforms to the conventions of Standard English?",
        
         answers: [
-            { text: "The first sentence of the first paragraph (“The recovery … Survey”)", correct: false},
-            { text: "The first sentence of the second paragraph (“There’s … program”)", correct: true},
-            { text: "The fourth paragraph (“While … areas”)", correct: false},
-            { text: "The fifth paragraph (“Since … years”)", correct: false},
+            { text: "treaty. Enabling", correct: false},
+            { text: "treaty, enabling", correct: true},
+            { text: "treaty; enabling", correct: false},
+            { text: "treaty — enabling", correct: false},
         ]
     },   
     {
-        question: "Question 3",
+        question: "Cats possess a remarkable ability that often goes unnoticed — their exceptional night vision. This unique trait is owed to the intricate structure of their eyes; cats' retinas are equipped with a higher percentage of rod cells — specialized photoreceptor cells sensitive to low light, compared to humans. This adaptation enables them to perceive objects in light levels six times dimmer than what a human requires to see clearly. As the sun sets and darkness envelops the landscape, these crepuscular creatures (animals more active during twilight hours) come alive, effortlessly navigating their surroundings with precision. <br/> Which choice best states the main idea of the text?",
         answers: [
-            { text: "stimulate", correct: true},
-            { text: "reinforce", correct: false},
-            { text: "support", correct: false},
-            { text: "provide", correct: false},
+            { text: "Cats’ retinas are more sensitive to low light, allowing them to see clearly in dimmer surroundings.", correct: false},
+            { text: "Cats owe their night vision to their retinas which contain more rod cells than humans, enabling them to see in dim light levels.", correct: true},
+            { text: "Crepuscular animals like cats are naturally more active during low light hours and hence require night vision to help them navigate better.", correct: false},
+            { text: "A higher number of rod cells makes a cat’s retina more sensitive to light.", correct: false},
         ]
     },
     {
-        question: "Question 4",
+        question: "A group of scientists were studying the growth of bacteria. It is their hope that they will be able to induce the bacteria to grow and metabolize oil as a food source. They have taken three samples of Escherichia coli and are growing them on nutrient agar plates. The scientists used three conditions to test the E. coli bacteria. The first group was grown at 37°C on plain nutrient agar plates. The second group was grown at 37°C on plain nutrient agar plates with a 5% oil solution. The third group was grown at 37°C on plain nutrient agar plates with a 20% oil solution.",
         answers: [
-            { text: "provide hypotheses that would explain the results of an ongoing study.", correct: false},
-            { text: "discredit the findings of a controversial experiment.", correct: false},
-            { text: "suggest an alternative interpretation of a surprising discovery.", correct: false},
-            { text: "encourage further study of sea otters to resolve a discrepancy.", correct: true},
+            { text: "The type of bacteria used.", correct: false},
+            { text: "The nutrient agar.", correct: false},
+            { text: "The number of days the bacteria grew.", correct: false},
+            { text: "The nutrient agar with oil.", correct: true},
         ]
     }
 ];
