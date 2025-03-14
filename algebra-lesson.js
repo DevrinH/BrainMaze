8c5b971e876ef736fb002b0e95ef2790238d4f7a
brainmaze <devrin.hoefer@gmail.com>
2025-03-13 22:08:54 -0700

    yes

--- algebra-lesson.js
+++ algebra-lesson.js
@@ -239,7 +239,7 @@ function gradeQuiz() {
     console.log(`Quiz score: ${percentage}%`);
     
     localStorage.setItem("quizPercentage", percentage); // Store percentage in localStorage
-    window.location.href = "results.html"; // Redirect to results page
+    window.location.href = "user-profile.html"; // Redirect to results page
 }
 
 
